735fd13413661ef30da65a85c6c6c675a88acfa2
Rian Saputra <riansptr120805@gmail.com>
2026-09-17 15:04:05 +0700

    fixx

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,5 +1,5 @@
 <nav>
-    <div class="brand">📚 Perpustakaan Digital Kampus</div>
+    <div class="brand">Perpustakaan Digital Kampus</div>
     <ul>
         <li><a href="{{ route('books.index') }}" class="{{ request()->routeIs('books.*') ? 'active' : '' }}">Buku</a></li>
         <li><a href="{{ route('categories.index') }}" class="{{ request()->routeIs('categories.*') ? 'active' : '' }}">Kategori</a></li>
